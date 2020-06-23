0553997419cae274699f0634f3dc04da9b80c817
Sam Harper <sam.j.harper@gmail.com>
2020-06-23 10:50:41 +0200

    speedups

--- test/pileupChecker.py
+++ test/pileupChecker.py
@@ -28,18 +28,26 @@ if __name__ == "__main__":
     in_filenames = CoreTools.get_filenames(args.in_filename,args.prefix)
     events = Events(in_filenames)
     
-    uniq_pu_evts = set()
-    for event in events:
+    uniq_pu_evts = {}
+    nr_dup_evts = 0
+    for eventnr,event in enumerate(events):
+        if eventnr%1000==0:
+            print("processing {} / {}".format(eventnr,events.size()))
+        if eventnr>10000:
+            break
         evtdata.get_handles(event)
         pu_sum  = evtdata.get("pu_sum")
         for pu_bx in pu_sum:
             evt_ids = pu_bx.getPU_EventID()
             for evt_id in evt_ids:
-                evt_id_tupl = make_id_tupl(evt_id)
-                if evt_id_tupl in uniq_pu_evts:
-                    print("PU event : ",evt_id_tupl," already used")
-                uniq_pu_evts.add(evt_id_tupl)
+                if evt_id.luminosityBlock() not in uniq_pu_evts:
+                    uniq_pu_evts[evt_id.luminosityBlock()] = set()
+                
+                if evt_id.event() in uniq_pu_evts[evt_id.luminosityBlock()]:
+                    nr_dup_evts += 1
+                else:
+                    uniq_pu_evts[evt_id.luminosityBlock()].add(evt_id.event())
 
-    expected_unique_events = events.size()*200*7
+    expect_uniq_evts = events.size()*200*7
 
-    print("number of events",events.size(),"number of unique PU events",len(uniq_pu_evts)," expected unique events",expected_unique_events)
+    print("number of events",events.size(),"number of unique PU events",len(uniq_pu_evts)," expected unique events",expect_uniq_evts," number dups ",nr_dup_evts)
