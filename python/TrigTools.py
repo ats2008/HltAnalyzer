df82dd943ef0faeb51f95419ba33f7257e145012
Sam Harper <sam.j.harper@gmail.com>
2021-01-11 16:47:37 +0100

    adding tools to measure the rate

--- python/TrigTools.py
+++ python/TrigTools.py
@@ -3,6 +3,8 @@ from __future__ import division
 from __future__ import print_function
 
 import ROOT
+import json
+import math
 
 def get_trig_indx(selected_name,trig_names):
     """
@@ -44,3 +46,48 @@ class TrigResults:
             return self.trig_res[trig]
         else:
             return False
+
+class MenuPathRates:
+    """
+    small class to sum over the rates for a HLT menu for each path
+    it assumes only a single menu is present, ie all events passed to 
+    it have the same trigger menu
+    """
+    class TrigData:
+        def __init__(self,indx,name):
+            self.indx = indx
+            self.name = str(name)
+            self.counts = 0
+            self.weights = 0.
+            self.weights_sq = 0.
+        
+    def __init__(self,input_weights):
+        self.trigs = []
+        with open(args.weights) as f:
+            weights = json.load(f)
+        self.weight_calc = EvtData.EvtWeightsV2(input_dict=weights)
+
+    def set_trigs(evtdata):
+        trig_names = evtdata.event.object().triggerNames(trig_res)
+        for indx,name in enumerate(trig_names):
+            self.trigs.append(TrigData(indx,name))
+            
+    def fill(self,evtdata):
+        if not self.trigs:
+            set_trigs(evtdata)
+        weight =weight_calc.weight_from_evt(evtdata)
+        trig_res = evtdata.get("trig_res")
+        for trig in self.trigs:
+            if trig_res[trig.indx].accept():
+                trig.counts += 1
+                trig.weights += weight
+                trig.weights_sq += weight_sq
+            
+    def get_results():
+        results = {}
+        for trig in self.trigs:
+            results[name] = {"rate" : trig.weights,
+                             "rate_err" : math.sqrt(trig.weights_sq),
+                             "raw_counts" : trig.counts}
+        return results
+
