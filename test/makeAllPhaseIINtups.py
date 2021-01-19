10a956c9e6cf533f4d4079c1336a957dcdba084e
Sam Harper <sam.j.harper@gmail.com>
2021-01-19 19:49:52 +0100

    minor hacks to read lists rather than directories

--- test/makeAllPhaseIINtups.py
+++ test/makeAllPhaseIINtups.py
@@ -7,6 +7,8 @@ import os
 import sys
 import glob
 
+import Analysis.HLTAnalyserPy.CoreTools as CoreTools
+
 def main():
     """
     a simple script to quickly ntuplise a large set of samples from EDM
@@ -30,7 +32,7 @@ def main():
     dirs_to_run = []
     print("going to process the following dirs")    
     for dir_ in args.input_dirs:
-        if os.path.isdir(dir_):
+        if os.path.isdir(dir_) or dir_.endswith(".list"):
             dirs_to_run.append(dir_)
             print(dir_)
         else:
@@ -39,20 +41,24 @@ def main():
     script_dir = os.path.dirname(__file__)
     ntup_script = os.path.join(script_dir,"makePhaseIINtup.py")
     run_script = os.path.join(script_dir,"runMultiThreaded.py")
-    for dir_ in dirs_to_run:
-                  
+    for dir_ in dirs_to_run:        
         head,tail = os.path.split(dir_.rstrip("/"))
         base_name = tail if tail else head
         out_file = os.path.join(args.out_dir,"{}.root".format(base_name))
+        out_file = out_file.replace(".list.root",".root")
         
-
-        ntupcmd = "python {ntup_script} --min_et {a.min_et} -r 5000 -w {a.weights}".format(ntup_script=ntup_script,a=args)
-        input_files = glob.glob(os.path.join(dir_,"*.root"))
+        weights_arg = "-w {}".format(args.weights) if args.weights else ""
+        ntupcmd = "python {ntup_script} --min_et {a.min_et} -r 5000 {weights_arg}".format(ntup_script=ntup_script,a=args,weights_arg=weights_arg)
+        if dir_.endswith(".list"):
+            input_files = CoreTools.get_filenames([dir_])
+        else:
+            input_files = glob.glob(os.path.join(dir_,"*.root"))
 
         runcmd = ["python",run_script,"-o",out_file,"--cmd",ntupcmd,"--hadd","-t",str(args.nr_threads),"-j",str(args.nr_threads)]
         runcmd.extend(input_files)
         
         print("running {}".format(dir_))
+       
         subprocess.Popen(runcmd).communicate()
     print("all done")
         
